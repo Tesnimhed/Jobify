1f79835f5b96f3a8f49917ed4de74dc510ecfcee
KenzaAsmani-dev <kenzaasmani8@gmail.com>
2025-02-01 12:08:26 +0100

    morgan installation and add type: module

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
+
+app.use(express.json());   
 app.get('/', (req,res) => {  console.log(req);    });
 
 app.listen('5000', () =>{console.log('Listening the port 5000')});
