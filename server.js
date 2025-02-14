2a8a0370c05659ee2583c9bffd2d757f22d4e8ec
KenzaAsmani-dev <kenzaasmani8@gmail.com>
2025-02-14 20:17:48 +0100

    Start with endpoints

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 const app = express();
+import morgan from 'morgan'; 
+import { nanoid } from 'nanoid';
 
 
-app.use(express.json());   
-app.get('/', (req,res) => {  console.log(req);    });
 
-app.listen('5000', () =>{console.log('Listening the port 5000')});
+
+let jobs= [
+{id: nanoid(), company: 'google', position: 'qa tester'},    
+{id: nanoid(), company: 'youtube', position: 'frontend developer'},    
+];
+
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+app.use(express.json());
+
+// Get all jobs
+app.get('/api/v1/jobs', (req,res)=>{  res.status(200).json({jobs})   })
+
+// Create a new job
+app.post('/api/v1/jobs', (req,res)=>{  
+
+const {company, position}= req.body; 
+
+if(!company || !position){
+    res.status(400).json({ message : 'Veuillez nous envoyer l entreprise et la position'});
+    return;
+}
+
+const id = nanoid(10); 
+const newJob= {id,company,position}; 
+
+jobs.push(newJob); 
+
+res.status(201).json({jobs}); 
+})
+
+
+const PORT= process.env.PORT || 5100; 
+app.get('/', (req,res) => {  res.send('salut')   });
+
+app.listen(PORT, () =>{console.log(`Listening the port ${PORT}`)});
